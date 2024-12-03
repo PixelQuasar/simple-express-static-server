f80014ac2c8ec271ee11e839b3092bf6752236e0
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2024-12-03 21:49:22 +0300

    minor fixes

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Используем официальный образ Node.js
+FROM node:18
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 3000
+
+CMD ["npm", "run", "start"]
---
+++ server.js
@@ -0,0 +1,26 @@
+/**
+ * SERVER CODE
+ */
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const PORT = 3000;
+const publicDir = path.join(__dirname, "public");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function (req, _, next) {
+    console.info(`${new Date().toISOString()} request ${req.url}`);
+    next();
+});
+app.use(express.static(publicDir));
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(publicDir, "404.html"));
+});
+
+app.listen(PORT, () => {
+    console.info(`Server successfully started on port ${PORT} (visit http://localhost:${PORT})`);
+});
